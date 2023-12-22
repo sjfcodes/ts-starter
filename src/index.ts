06f088ff4b0f035f22e94779e4fe9a35afda8efd
sjfox <sjfcodes@gmail.com>
2023-12-21 17:56:11 -0800

    adds express server boiler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import "dotenv/config";
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
