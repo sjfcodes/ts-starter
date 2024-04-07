b346294cc77e38ee3ad08cc85a432f73544218a1
sjfox <sjfcodes@gmail.com>
2024-04-06 17:18:55 -0700

    wip eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+}
+];
